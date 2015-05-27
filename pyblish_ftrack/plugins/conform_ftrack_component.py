bd03dbcaaed629154cd9574b031ed5032a4cff66
Milan Kolar <mail@mkolar.com>
2015-05-27 16:48:51 +0200

    updated docstring and removed error checking (this is taken care of in validation)

--- pyblish_ftrack/plugins/conform_ftrack_component.py
+++ pyblish_ftrack/plugins/conform_ftrack_component.py
@@ -4,14 +4,13 @@ import ftrack
 
 @pyblish.api.log
 class FtrackUploadComponent(pyblish.api.Conformer):
-    """Creates component within supplied version.
+    """ Creates component within supplied version.
 
-    Expected data members:
-    'ftrackComponent' - path that will be saved as a component
-    'ftrackComponentName' - component name
-    'ftrackVersionID' - ID of a version where component should be created
-    Optional:
-    'ftrackReviewable' - will make current instance reviewable
+        Arguments:
+            ftrackComponent (string): path that will be saved as a component
+            ftrackComponentName (string): component name
+            ftrackVersionID (string): ID of a version where component should be created
+            ftrackReviewable (string, optional): will make
     """
 
     order = pyblish.api.Conformer.order + 0.11
@@ -23,20 +22,12 @@ class FtrackUploadComponent(pyblish.api.Conformer):
     def process_instance(self, instance):
 
         if instance.has_data('ftrackComponent') and instance.has_data('ftrackComponentName'):
-
             sourcePath = os.path.normpath(instance.data('ftrackComponent'))
 
             if instance.context.has_data('ftrackVersionID'):
                 version = ftrack.AssetVersion(id=instance.context.data('ftrackVersionID'))
-
                 componentName = instance.data('ftrackComponentName')
-
-                try:
-                    component = version.getComponent(name=componentName)
-                    component.delete()
-                    self.log.info('Replacing component with name "%s"' % componentName)
-                except:
-                    self.log.info('Creating component with name "%s"' % componentName)
+                self.log.info('creating component {}'.format(componentName))
 
                 version.createComponent(name=componentName, path=sourcePath)
                 # make reviewable
