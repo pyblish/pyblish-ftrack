5165841561633f8c1274177b5b492d4bb7a2ccec
Milan Kolar <mail@mkolar.com>
2015-06-26 17:08:26 +0200

    fix mixed up merges

--- pyblish_ftrack/plugins/collect_ftrack_data.py
+++ pyblish_ftrack/plugins/collect_ftrack_data.py
@@ -3,7 +3,6 @@ import json
 import base64
 import sys
 import ftrack
-import pyblish_qml
 import pyblish.api
 
 
@@ -29,75 +28,8 @@ class CollectFtrackData(pyblish.api.Selector):
         taskid = decodedEventData.get('selection')[0]['entityId']
         ftrack_data = self.get_data(taskid)
 
-        self.log.debug(str(ftrack_data))
-        # Get ftrack Asset
-        task = ftrack.Task(taskid)
-
-        try:
-            parent = ftrack.Shot(id=ftrack_data['Shot']['id'])
-        except:
-            parent = ftrack.Shot(id=ftrack_data['Asset_Build']['id'])
-
-        asset_type = ftrack_data['Task']['code']
-
-        if asset_type == 'light':
-            asset_type = 'render'
-
-        asset_name = ftrack_data['Task']['type']
-
-        assets = task.getAssets(assetTypes=[asset_type])
-
-        if assets:
-            for a in assets:
-                if a.getName() == asset_name:
-                    self.log.info('Found existing asset with name\
-                    {}'.format(a.getName()))
-                    asset = a
-                    break
-                else:
-                    self.log.info('Found existing asset with name\
-                    {}'.format(a.getName()))
-                    asset = a
-        else:
-            self.log.info('Creating new asset')
-            asset = parent.createAsset(name=asset_name,
-                                       assetType=asset_type,
-                                       task=task)
-
-        self.log.info('Using ftrack asset {}'.format(asset.getName()))
-
-        ftrack_data['Asset'] = {'id': asset.getId(),
-                                'name': asset.getName()
-                                }
-
-        # Get ftrack AssetVersion or set 'createFtrackVersion' argument
-        if context.data('version'):
-            version_number = int(context.data('version'))
-            version = None
-
-            for v in asset.getVersions():
-                if int(v.getVersion()) == version_number:
-                    version = v
-                    ftrack_data['AssetVersion'] = {'id': version.getId(),
-                                                   'number': version_number,
-                                                   }
-                    self.log.warning('This version already exists. Will\
-                                     check for existence of  components')
-
-            if not version:
-                context.set_data('createFtrackVersion', value=True)
-                self.log.debug('Setting createFtrackVersion arguments')
-        else:
-            context.set_data('createFtrackVersion', value=True)
-            self.log.debug('Setting createFtrackVersion arguments')
-            self.log.warning('Missing version number in context.')
-
         # set ftrack data
         context.set_data('ftrackData', value=ftrack_data)
-
-        context.set_data("label", "The World")
-
-        pyblish_qml.settings.WindowTitle = 'testin path/ shot/ task'
 
         self.log.info('Found ftrack data: \n\n%s' % ftrack_data)
 
